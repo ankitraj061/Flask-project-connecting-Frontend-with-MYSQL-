a8db036f4975c8812a9192cc5a78cd261e19537e
ankitraj061 <ankitraj07032002@gmail.com>
2024-03-08 23:29:11 +0530

    successfully created

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Ankit@12'
+app.config['MYSQL_DB'] = 'student_registration'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def registration_form():
+    return render_template('registration_form.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        age = request.form['age']
+        father_name = request.form['father_name']
+        mother_name = request.form['mother_name']
+        phone_no = request.form['phone_no']
+        DOB = request.form['DOB']
+        blood_group = request.form['blood_group']
+        address = request.form['address']
+        department = request.form['department']
+        course = request.form['course']
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO students (name, email, age, father_name, mother_name, phone_no, DOB, blood_group, address, department, course) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    (name, email, age, father_name, mother_name, phone_no, DOB, blood_group, address, department, course))
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('registration_form'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
